bc60c18e445569fad480c8ad174dc28854e1d119
douglas-front <douglasjknw007@gmail.com>
2024-06-21 11:54:05 -0300

    feat: nav and contact responsive

--- src/app/(contact)/contact/Layouts/Nav/Nav.tsx
+++ src/app/(contact)/contact/Layouts/Nav/Nav.tsx
@@ -1,23 +1,23 @@
-"use client"
+"use client";
 
 import Arrow from "@/common/components/Arrow";
 import Logo from "@/common/components/Logo";
-import styles from "./Nav.module.css"
-import { useRouter } from "next/navigation"
-
+import styles from "./Nav.module.css";
+import { useRouter } from "next/navigation";
 
 export default function Nav() {
+  const router = useRouter();
 
-    const router = useRouter()
+  const backToHome = () => router.push("/");
 
-    const backToHome = () => router.push("/")
-
-    return(
-        <nav className={`${styles.nav} row container`}>
-            <Logo/>
-            <button className={styles.backHome} onClick={backToHome}>
-                <Arrow/>
-            </button>
-        </nav>
-    )
-}   
+  return (
+    <nav className={`${styles.nav} row container`}>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <button className={styles.backHome} onClick={backToHome}>
+        <Arrow />
+      </button>
+    </nav>
+  );
+}
